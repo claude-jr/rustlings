8aabcff3174bd48eb97b6b751ae118d51417bc10
Claude Djoumessi <djoumessiclaude95@gmail.com>
2026-06-24 16:18:54 +0100

    chap_01: variables

--- exercises/01_variables/variables5.rs
+++ exercises/01_variables/variables5.rs
@@ -3,6 +3,9 @@ fn main() {
     println!("Spell a number: {number}");
 
     // TODO: Fix the compiler error by changing the line below without renaming the variable.
-    number = 3;
+
+    // <author>: this is shadowing. like replacing an existing value with a variable name
+    //           that exists without types concern.
+    let number = 3;
     println!("Number plus two is: {}", number + 2);
 }
--- exercises/01_variables/variables6.rs
+++ exercises/01_variables/variables6.rs
@@ -1,6 +1,5 @@
 // TODO: Change the line below to fix the compiler error.
-const NUMBER = 3;
-
+const NUMBER: i32 = 0;
 fn main() {
     println!("Number: {NUMBER}");
 }
