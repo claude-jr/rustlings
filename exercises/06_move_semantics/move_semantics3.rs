2f05b4c33347e896e189a68f1ba1a5366ea2216d
Claude Djoumessi <djoumessiclaude95@gmail.com>
2026-06-24 22:08:27 +0100

    chap_06: move_semantics, references, ownerships

--- exercises/06_move_semantics/move_semantics3.rs
+++ exercises/06_move_semantics/move_semantics3.rs
@@ -1,5 +1,5 @@
 // TODO: Fix the compiler error in the function without adding any new line.
-fn fill_vec(vec: Vec<i32>) -> Vec<i32> {
+fn fill_vec(mut vec: Vec<i32>) -> Vec<i32> {
     vec.push(88);
 
     vec
@@ -18,5 +18,10 @@ mod tests {
         let vec0 = vec![22, 44, 66];
         let vec1 = fill_vec(vec0);
         assert_eq!(vec1, [22, 44, 66, 88]);
+
+        // Two things to note
+        // 1 - vec0 is dead since ownership went to fill_vec
+        // 2 - vec1 modifies the value itself of vec0 and returning the
+        //     transformed value.
     }
 }
